e554787f8b769e0e90bc7e2d0212fc7b73fdc4b0
Paul Krupski <pjkrupski@gmail.com>
2022-10-26 01:09:45 -0400

    with open param to fix unicode parse error, regex added to ensure blank sentences are excluded

--- reader.py
+++ reader.py
@@ -1,18 +1,22 @@
 import PyPDF2
 import csv
+import re
 
 def scan_pdf(key_words):
     col = []
     rows = []
-    pdf = open('SamplePDF.pdf', 'rb')
+    sentance = []
+    sentances = []
+    blank = re.compile('[^\s]')
+    #Hashmap will contain k v pairs that are ("word", [pg, (i, j), (k, l)])
+    #Each key will map to a list of (i, j)s of where in the page that word occurs
+    word_map = {}
+    pdf = open('SamplePDF2.pdf', 'rb')
     reader = PyPDF2.PdfFileReader(pdf)
     #Load each page thats been parsed from the pdf
     for pg in range(1, reader.numPages+1):
-        page = reader.getPage(0)
+        page = reader.getPage(pg-1)
         content = page.extractText()
-        #Hashmap will contain k v pairs that are ("word", [pg, (i, j), (k, l)])
-        #Each key will map to a list of (i, j)s of where in the page that word occurs
-        word_map = {}
         line = []
         words_on_page = []
         #Split up lines based on newline character
@@ -25,22 +29,24 @@ def scan_pdf(key_words):
         #Split up words within each line based on ' ' character
         word = ""
         word_count = 0
-        sentance = []
-        sentances = []
         for i in range(0, len(words_on_page)):
             for j in range (0, len(words_on_page[i])):
+                #Split words on blank space
                 if words_on_page[i][j] == ' ':
                    sentance.append(word)
                    #If word exists in map, update the corrdinates list to store the next occurance location
                    if word in word_map.keys():
-                       word_map.get(word).append([pg, i, word_count])
+                       #changed to store which sentance word appears in relation to entire doc instead of local page
+                       word_map.get(word).append([pg, len(sentances), word_count])
                    else:
                        word_map[word] = [[pg, i, word_count]]
                    word = ""
                    word_count += 1
                    continue
                 word += words_on_page[i][j]
-            sentances.append(sentance)
+            #Ensure sentance isnt a blank line
+            if blank.match("".join(sentance)):
+                sentances.append(sentance)
             sentance = []
             word_count = 0
     #Search Words
@@ -48,10 +54,10 @@ def scan_pdf(key_words):
         if wd in word_map.keys():
             col.append(str(wd) + " appears " + str(len(word_map[wd])) + " times in the doc at the following locations")
             rows.append(col)
-            rows.append(" ")
             col = []
+            #Record neighboring sentances for each occurance of the word
             for ocr in word_map[wd]:
-                col.append("Page " + str(ocr[0]) + " at sentance " + str(ocr[1]+1) + " word " + str(ocr[2]+1))
+                col.append("Page " + str(ocr[0]) + " at doc sentance # " + str(ocr[1]+1) + " pg word # " + str(ocr[2]+1))
                 #Leading sentance
                 if(ocr[1] > 0):
                     col.append("Previous Sentance: " + " ".join(sentances[ocr[1] - 1]))
@@ -62,15 +68,13 @@ def scan_pdf(key_words):
                     col.append("Following Sentance:  " + " ".join(sentances[ocr[1] + 1]))
                 #Add space between words
                 rows.append(col)
-                rows.append(" ")
                 #Reset col for storing info on the next occurance
                 col = []
         else:
             col.append(str(wd) + " does not exist ")
     pdf.close()
-    print(rows)
     #Export to CSV
-    with open("Search_Results.csv", "w") as my_csv:
+    with open("Search_Results.csv", "w", encoding="utf-8") as my_csv:
         csvWriter = csv.writer(my_csv)
         csvWriter.writerows(rows)
         my_csv.close
